0d853aadeefc3d75a5416d1e59176abbf195106d
Zahra Moradi <zahraaa.moradi78@gmail.com>
2018-12-18 11:39:26 +0330

    add files

--- a.py
+++ a.py
@@ -1,16 +1,14 @@
-p = input('enter your text:')
 n = 10
 s = n
-if p == 'a':
-    for i in range(0, n):
-        for j in range(s-1, 0, -1):
-            print(' ', end='')
-        for k in range(i):
-            if k == 0 or k == i-1:
-                print(' *', end='')
-            elif i == n/2+1:
-                print(' *', end='')
-            else:
-                print('  ', end='')
-        s = s-1
-        print()
+for i in range(0, n):
+    for j in range(s-1, 0, -1):
+        print(' ', end='')
+    for k in range(i):
+        if k == 0 or k == i-1:
+            print(' *', end='')
+        elif i == n/2+1:
+            print(' *', end='')
+        else:
+            print('  ', end='')
+    s = s-1
+    print()
--- b.py
+++ b.py
@@ -1,19 +1,128 @@
-n = int(input('enter your number:'))
-print('*')
-for i in range(-1, n):
-    print('*' + ' ' * (i + 2) + '*')
-for i in range(n, -1, -1):
-    print('*' + ' ' * (i + 1) + '*')
-print('*')
-n += 4
-for i in range(0, n):
-    print('*' + ' ' * (i + 2) + '*')
-    if i == n-1:
-        print('*' + ' ' * (i + 2) + '*')
-        pass
-for i in range(n, -1, -1):
-    print('*' + ' ' * (i + 1) + '*')
-print('*')
+n = 4
+s = n
+for i in range(n):
 
+    for j in range(n):
+        if j == 0:
+            print('*', end='')
+    for p in range(2*i-1):
+        print(' ', end='')
+    for k in range(i):
+        if k == i-1:
+            print('*', end='')
+
+    if i == n-1:
+        print()
+        for j in range(n):
+            if j == 0:
+                print('*', end='')
+        for p in range(2 * i - 1):
+            print(' ', end='')
+        for k in range(i):
+            if k == i - 1:
+                print(' *', end='')
+        print()
+
+        for j in range(n):
+            if j == 0:
+                print('*', end='')
+        for p in range(2 * i - 1):
+            print(' ', end='')
+
+        for k in range(i):
+            if k == i - 1:
+                print('  *', end='')
+    print()
+
+for i in range(n):
+
+    if i == 1:
+        for j in range(s):
+            if j == 0:
+                print('*', end='')
+        for p in range(2 * s - 1):
+            print(' ', end='')
+
+        for k in range(s):
+            if k == s - 1:
+                print(' *', end='')
+        print()
+
+    for j in range(s):
+        if j == 0:
+            print('*', end='')
+    for p in range(2*s-1):
+        print(' ', end='')
+    for k in range(s):
+        if k == s-1:
+            print('*', end='')
+    s = s-1
+    print()
+
+s = n+2
+for i in range(n+2):
+    if i == 1:
+        print('* *')
+    for j in range(n+2):
+        if j == 0:
+            print('*', end='')
+    for p in range(2*i+1):
+        print(' ', end='')
+    for k in range(i):
+        if k == i-1:
+            print('*', end='')
+
+    if i == n+1:
+        print()
+        for j in range(n+2):
+            if j == 0:
+                print('*', end='')
+        for p in range(2 * i+1):
+            print(' ', end='')
+
+        for k in range(i):
+            if k == i - 1:
+                print(' *', end='')
+        print()
+
+        for j in range(n+2):
+            if j == 0:
+                print('*', end='')
+        for p in range(2 * i+1):
+            print(' ', end='')
+        for k in range(i):
+            if k == i - 1:
+                print('  *', end='')
+    print()
+
+for i in range(n+2):
+
+    if i == 1:
+        for j in range(s):
+            if j == 0:
+                print('*', end='')
+        for p in range(2 * s+1):
+            print(' ', end='')
+        for k in range(s):
+            if k == s - 1:
+                print(' *', end='')
+        print()
+
+    for j in range(s):
+        if j == 0:
+            print('*', end='')
+    for p in range(2*s+1):
+        print(' ', end='')
+    for k in range(s):
+        if k == s-1:
+            print('*', end='')
+
+    if i == n+1:
+        print()
+        print('* *', end='')
+    s = s-1
+    print()
+print('*')
+s = n
 
 
